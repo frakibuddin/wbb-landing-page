f46d38d8be2f6c59808f55d31da4cb9d40c49748
frakibuddin <frakibuddin360@gmail.com>
2023-09-16 01:32:27 +0600

    updated

--- js/script.js
+++ js/script.js
@@ -2,27 +2,50 @@ $(document).ready(function () {
   var owl = $("#owl-demo");
 
   owl.owlCarousel({
-    items: 3, //10 items above 1000px browser width
-    itemsDesktop: [1000, 2], //5 items between 1000px and 901px
-    itemsDesktopSmall: [900, 2], // betweem 900px and 601px
-    itemsTablet: [600, 1], //2 items between 600 and 0
-    itemsMobile: false, // itemsMobile disabled - inherit from itemsTablet option
+    items: 3,
+    itemsDesktop: [1000, 2],
+    itemsDesktopSmall: [900, 2],
+    itemsTablet: [600, 1],
+    itemsMobile: false,
   });
 
   var owl_gallery = $("#owl-gallery");
 
   owl_gallery.owlCarousel({
-    items: 3, //10 items above 1000px browser width
-    itemsDesktop: [1000, 2], //5 items between 1000px and 901px
-    itemsDesktopSmall: [900, 2], // betweem 900px and 601px
-    itemsTablet: [600, 1], //2 items between 600 and 0
-    itemsMobile: false, // itemsMobile disabled - inherit from itemsTablet option
+    items: 3,
+    itemsDesktop: [1000, 2],
+    itemsDesktopSmall: [900, 2],
+    itemsTablet: [600, 1],
+    itemsMobile: false,
   });
-  // Custom Navigation Events
   $(".next").click(function () {
     owl_gallery.trigger("owl.next");
   });
   $(".prev").click(function () {
     owl_gallery.trigger("owl.prev");
+  });
+
+  var owl_gallery_single = $("#owl-gallery-single");
+
+  owl_gallery_single.owlCarousel({
+    items: 1,
+    itemsDesktop: [1000, 1],
+    itemsMobile: false,
+  });
+
+  $(".next").click(function () {
+    owl_gallery_single.trigger("owl.next");
+  });
+  $(".prev").click(function () {
+    owl_gallery_single.trigger("owl.prev");
+  });
+
+  // owl-demo-single-sm
+  var owl_single_sm = $("#owl-demo-single-sm");
+
+  owl_single_sm.owlCarousel({
+    items: 1,
+    itemsDesktop: [1000, 1],
+    itemsMobile: false,
   });
 });
