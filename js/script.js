5768cb4ff6611821d5266ce957450453151ae283
frakibuddin <frakibuddin360@gmail.com>
2023-09-17 14:48:24 +0600

    update final

--- js/script.js
+++ js/script.js
@@ -30,7 +30,9 @@ $(document).ready(function () {
   owl_gallery_single.owlCarousel({
     items: 1,
     itemsDesktop: [1000, 1],
-    itemsMobile: false,
+    itemsDesktopSmall: [900, 1],
+    itemsTablet: [600, 1],
+    itemsMobile: Float32Array,
   });
 
   $(".next").click(function () {
@@ -47,3 +49,83 @@ $(document).ready(function () {
     items: 1,
   });
 });
+
+// nav hover function dropdown-menu
+let dropDownLink = document.querySelectorAll("#dropDown-link");
+let dropdownMenu = document.querySelectorAll(".dropdown-menu");
+
+dropDownLink.forEach((element) => {
+  element.addEventListener("mouseenter", function () {
+    element.querySelector(".dropdown-menu").classList.add("show");
+  });
+  element.addEventListener("mouseleave", function () {
+    element.querySelector(".dropdown-menu").classList.remove("show");
+  });
+});
+
+//omload popup show
+// $(window).on("load", function () {
+//   setTimeout(() => {
+//     $(".modal").modal("show");
+//   }, 3000);
+// });
+
+// onscroll animation
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.15)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      //hideScrollElement(el);
+    }
+  });
+};
+
+window.addEventListener("scroll", () => {
+  handleScrollAnimation();
+});
+
+//tap to top btn
+const tap_top_btn = document.querySelector(".tap-toTop");
+
+window.onscroll = function () {
+  let posHight = document.documentElement.scrollHeight;
+  let pos = window.scrollY;
+
+  if (pos > posHight - 2500) {
+    tap_top_btn.classList.add("active");
+  } else {
+    tap_top_btn.classList.remove("active");
+  }
+};
+
+tap_top_btn.onclick = function () {
+  document.documentElement.scrollTop = 0;
+};
